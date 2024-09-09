29a2b601f59e979f0bd633d08d2b33cac11369ad
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-09 14:12:28 +0545

    Tweak some functional component

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -3,4 +3,4 @@ function Greeting() {
   }
   
   export default Greeting;
-  
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
+import Food from "./Food.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Greeting />
+    <Food/>
   </StrictMode>,
 );
