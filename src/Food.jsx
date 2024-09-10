f423a5cb781bfa6a6b3ab408daf682d65d4b1237
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-10 11:41:38 +0545

    Add contitional rendering

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,5 +1,28 @@
-function Food() {
-    return <h1>&quot;My favorite food is Gimbap&quot;</h1>;
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">{name} ✅</li>;
   }
-  
-  export default Food;
+  return <li className="item">{name}</li>;
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item 
+          isPacked={true} 
+          name="Space suit" 
+        />
+        <Item 
+          isPacked={true} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          isPacked={false} 
+          name="Photo of Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
-import Food from "./Food.jsx";
+import Item from "./Food.jsx";
 import "./index.css";
+import PackingList from "./Food.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Greeting />
-    <Food/>
+    <PackingList/>
   </StrictMode>,
 );
