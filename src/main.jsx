f48dd57633ffe426fc933646a53c4c294ad5d676
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-11 11:29:38 +0545

    Add Person functional component to update age

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@ import Item from "./Food.jsx";
 import "./index.css";
 import PackingList from "./Food.jsx";
 import FeedbackForm from "./Practice.jsx";
+import Person from "./Person.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Greeting />
     <PackingList/>
     <FeedbackForm/>
+    <Person/>
   </StrictMode>,
 );
