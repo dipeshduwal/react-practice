d03f30bacd26a19e0871d38fd14934381ef4f7a4
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-11 11:43:40 +0545

    Add input controlled components

--- src/Person.jsx
+++ src/Person.jsx
@@ -1,27 +1,4 @@
-export default function Person() {
-    const [person, setPerson] = useState({ name: "John", age: 100 });
-  
-    const handleIncreaseAge = () => {
-      console.log("in handleIncreaseAge (before setPerson call): ", person);
-      setPerson({ ...person, age: person.age + 1 });
-      // we've called setPerson, surely person has updated?
-      console.log("in handleIncreaseAge (after setPerson call): ", person);
-    };
-  
-    // this console.log runs every time the component renders
-    // what do you think this will print?
-    console.log("during render: ", person);
-  
-    return (
-      <>
-        <h1>{person.name}</h1>
-        <h2>{person.age}</h2>
-        <button onClick={handleIncreaseAge}>Increase age</button>
-      </>
-    );
-  }
-  
-  function FirstName() {
+function FirstName() {
     const [value, setValue] = useState("");
   
     return (
@@ -46,3 +23,20 @@ export default function Person() {
 
     );
   }
+
+export default function Person() {
+    const [person, setPerson] = useState({ fullname , age });
+    
+    const handleName = () => {
+        setName((fullname) => ({ ...fullname, name: {FirstName} + {LastName}}));
+    };
+
+    return (
+      <>
+        <h1>{person.fullname}</h1>
+        <h2>{person.age}</h2>
+        <button onClick={handleIncreaseAge}>Increase age</button>
+      </>
+    );
+  }
+  
